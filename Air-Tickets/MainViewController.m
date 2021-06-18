8dccd642b3723082dc11040706fd883db3f26106
Alexander Fomin <inzhener@outlook.com>
2021-06-18 16:15:17 +0300

    Добавил NavigationController

---
+++ Air-Tickets/MainViewController.m
@@ -0,0 +1,21 @@
+//
+//  MainViewController.m
+//  Air-Tickets
+//
+//  Created by Alexander Fomin on 18.06.2021.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor redColor]];
+}
+
+@end
